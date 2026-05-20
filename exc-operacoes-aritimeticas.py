69e436a731cd662eec5e2f1a8d7ed7546ea5329a
bordin010 <enzodominone@gmail.com>
2026-05-20 13:46:08 -0300

    Exercício03

---
+++ exc-operacoes-aritimeticas.py
@@ -0,0 +1,84 @@
+# EX1
+# Calcule o total de alunos em duas turmas:
+# uma com 28 alunos e outra com 32.
+
+turma = 28
+turma2 = 32
+print("O total de alunos juntando as duas salas é:", turma + turma2, "alunos!")
+print("\n\n")
+
+# EX2
+# Um pacote de dados tem 500MB.
+# Após baixar 150MB, quanto resta?
+
+pacote = int(input("Qual é o tamanho do pacote de dados?:"))
+print("Resto:", pacote - 150, "MB")
+print("\n\n")
+
+# EX3
+# Uma caixa contém 24 ovos.
+# Quantos ovos existem em 8 caixas?
+
+caixadeovos = 24
+print("8 caixas de ovos tem:", caixadeovos * 8, "ovos!")
+print("\n\n")
+
+# EX4
+# Divida 10 litros de suco igualmente
+# entre 3 pessoas e mostre o resultado.
+
+litrosdesuco = 10
+pessoas = 3
+print("Se dividirmos 10 litros de suco, para nós 3, teremos:", litrosdesuco / pessoas, "litros de suco para cada!")
+print("\n\n")
+
+# EX5
+# Use divisão inteira para descobrir
+# quantas pessoas recebem 3 litros inteiros
+# dos 10 litros disponíveis.
+
+litros = 10 
+por_pessoa = 3
+pessoas = litros // por_pessoa
+print("Quantidade de pessoa que recebem 3 litros inteiros é:", pessoas, ("pessoas!"))
+print("\n\n")
+
+# EX6
+# Calcule o resto da divisão de 10 por 3.
+
+resto = 10 % 3
+print("O resto da divisão é:", resto)
+print("\n\n")
+
+# EX7
+# Calcule 2 elevado à potência 8.
+
+print("2 elevado à 8 é:", 2 ** 8)
+print("\n\n")
+
+# EX8
+# Calcule a raiz quadrada de 81
+# usando exponenciação.
+
+numero = 81
+resultado = (81 ** (1/2))
+print("Resultado:", resultado)
+print("\n\n")
+
+# EX9
+# Calcule a expressão:
+# (10 + 5) * 2 - 8 / 4
+
+resultado = (10 + 5) * 2 - 8 / 4
+print("Resultado:", resultado)
+print("\n\n")
+
+# EX10
+# Mostre o resultado da divisão de -7 por 2
+# usando divisão inteira e o operador de resto.
+
+resultado = -7 // 2
+print("Resultado:", resultado)
+
+resultado2 = -7 % 2
+print("Resultado:", resultado)
