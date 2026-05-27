28edfbe7ec162991dab922a4ff812ae4bbff74e4
bordin010 <enzodominone@gmail.com>
2026-05-27 12:00:38 -0300

    Operações com strings

--- Modulo-02/2.1-manipular-texto-em-python.py
+++ Modulo-02/2.1-manipular-texto-em-python.py
@@ -67,3 +67,61 @@ print("Do início até 3:", st[:3])
 print ("Do 2 até o fim:", st[2:])
 
 print("Tamanho", len(st))
+
+
+# ---------------------------------------------
+# 5) OPERAÇÕES COM STRINGS
+# ---------------------------------------------
+# PYTHON PERMITE VÁRIAS OPERAÇÕES COM STRINGS
+
+
+print("m" in st)
+# SIGNIFICA QUE "X" NÃO EXISTE NA STRING
+
+print("m" * 20)
+# MULTIPLICAÇÃO REPETE A STRING
+
+print("m" + "aracana")
+# OPERADOR + CONCATENA STRINGS
+
+
+# ---------------------------------------------
+# 6) STRINGS SÃO IMUTÁVEIS
+# ---------------------------------------------
+# STRINGS NÃO PODEM SER ALTERADAS DIRETAMENTE!!!
+# ISSO SIGNIFICA QUE O CONTEÚDO ORIGINAL NÃO MUDA.
+# O QUE ACONTECE É A CRIAÇÃO DE UMA NOVA STRING.
+
+texto ="python 9"
+
+# MÉTODO REPLACE CRIA UMA NOVA STRING
+texto = texto.replace("9", "0")
+
+print(texto)
+
+# ---------------------------------------------
+# 7) MÉTODOS IMPORTANTES
+# ---------------------------------------------
+# STRINGS POSSUEM VÁRIOS MÉTODOS.
+
+cidade = "maracana"
+# COLOCA A PRIMEIRA LETRA EM MAIUSCULA.
+print(cidade.capitalize())
+
+# CONTA QUANTAS VEZES "A" APARECE
+print(cidade.count("a"))
+
+# VERIFICAR SE COMEÇA COM "M"
+print(cidade.startswith("m"))
+
+# VERICA SE TERMINA COM "Z"
+print(cidade.endswith("z"))
+
+frase = "copa de 2002"
+
+# DIVIDE A STRING EM UMA LISTA
+print(frase.split(" "))
+
+# ---------------------------------------------
+# 8) FORMATAÇÃO DE STRINGS
+# ---------------------------------------------
